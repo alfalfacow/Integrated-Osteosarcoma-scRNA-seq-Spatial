1782ff41af4d72e724c29c06657f339f59434271
elk012 <elk010@ucsd.edu>
2026-09-01 00:15:55 -0700

    Update 3-CellChat_Comparison_Between_Sites.R

--- 3-CellChat_Comparison_Between_Sites.R
+++ 3-CellChat_Comparison_Between_Sites.R
@@ -1,222 +1,32 @@
 ##Cell-Cell interactions using CellChat
 ##Setup: One CellChat object for each condition (primary vs metastatic), then compare
+##
 ##Tutorials:
 #https://htmlpreview.github.io/?https://github.com/sqjin/CellChat/blob/master/tutorial/Interface_with_other_single-cell_analysis_toolkits.html
 #https://htmlpreview.github.io/?https://github.com/sqjin/CellChat/blob/master/tutorial/CellChat-vignette.html
 #multiple comparisons:
 #https://htmlpreview.github.io/?https://github.com/sqjin/CellChat/blob/master/tutorial/Comparison_analysis_of_multiple_datasets.html
-#merging is ok:
-#https://github.com/jinworks/CellChat/issues/62
 
 
 library(CellChat)
-library(Seurat)
-library(dplyr)
-library(ggplot2)
 library(patchwork)
 library(ComplexHeatmap)
 library(grid)
+library(ggplot2)
 
 
-#A: Data entry and preparation
+#A: Load primary and metastatic CellChat objects
 
-#Split integrated Seurat object by tissue type
-metastatic <- subset(Integrated, subset = Tissue_Type == "lung_metastasis")
-primary <- subset(Integrated, subset = Tissue_Type == "primary")
-
-
-#Create metastatic CellChat object
-data.input <- GetAssayData(
-  metastatic,
-  assay = "RNA",
-  layer = "data"
-) #normalized data matrix
-
-labels <- metastatic$cell2loc_labels
-samples <- metastatic$sample
-
-meta <- data.frame(
-  labels = labels,
-  samples = samples,
-  row.names = names(labels)
+primary_CellChat <- readRDS(
+  "/Users/elizabethkim/osteosarcoma/cell chat/primary_CellChat.rds"
 )
 
-metastatic_CellChat <- createCellChat(
-  object = data.input,
-  meta = meta,
-  group.by = "labels"
+metastatic_CellChat <- readRDS(
+  "/Users/elizabethkim/osteosarcoma/cell chat/metastatic_CellChat.rds"
 )
 
 
-#Create primary CellChat object
-data.input <- GetAssayData(
-  primary,
-  assay = "RNA",
-  layer = "data"
-) #normalized data matrix
-
-labels <- primary$cell2loc_labels
-samples <- primary$sample
-
-meta <- data.frame(
-  group = labels,
-  samples = samples,
-  row.names = names(labels)
-)
-
-primary_CellChat <- createCellChat(
-  object = data.input,
-  meta = meta,
-  group.by = "group"
-)
-
-
-#B: Load CellChat database
-
-#Load human CellChat database
-CellChatDB <- CellChatDB.human
-
-#Look at database categories/interactions
-showDatabaseCategory(CellChatDB)
-dplyr::glimpse(CellChatDB$interaction)
-
-#Subset database to protein-mediated signaling categories
-CellChatDB.use <- subsetDB(
-  CellChatDB,
-  search = c(
-    "Secreted Signaling",
-    "ECM-Receptor",
-    "Cell-Cell Contact"
-  ),
-  key = "annotation"
-)
-
-#Assign filtered database to CellChat objects
-metastatic_CellChat@DB <- CellChatDB.use
-primary_CellChat@DB <- CellChatDB.use
-
-#Subset expression matrices to signaling genes in database
-metastatic_CellChat <- subsetData(metastatic_CellChat)
-primary_CellChat <- subsetData(primary_CellChat)
-
-
-#C: Identify overexpressed signaling genes/interactions
-
-metastatic_CellChat <- identifyOverExpressedGenes(
-  metastatic_CellChat
-)
-
-metastatic_CellChat <- identifyOverExpressedInteractions(
-  metastatic_CellChat
-)
-
-primary_CellChat <- identifyOverExpressedGenes(
-  primary_CellChat
-)
-
-primary_CellChat <- identifyOverExpressedInteractions(
-  primary_CellChat
-)
-
-
-#D: Compute communication probabilities
-
-#Main analysis uses triMean
-metastatic_CellChat <- computeCommunProb(
-  metastatic_CellChat,
-  type = "triMean"
-)
-
-primary_CellChat <- computeCommunProb(
-  primary_CellChat,
-  type = "triMean"
-)
-
-#Compute pathway-level communication probabilities
-metastatic_CellChat <- computeCommunProbPathway(
-  metastatic_CellChat
-)
-
-primary_CellChat <- computeCommunProbPathway(
-  primary_CellChat
-)
-
-#Filter communication involving cell populations with fewer than 10 cells
-metastatic_CellChat <- filterCommunication(
-  metastatic_CellChat,
-  min.cells = 10
-)
-
-primary_CellChat <- filterCommunication(
-  primary_CellChat,
-  min.cells = 10
-)
-
-
-#E: Extract communication tables
-
-#All inferred ligand-receptor interactions
-met_paths <- subsetCommunication(
-  metastatic_CellChat
-)
-
-prim_paths <- subsetCommunication(
-  primary_CellChat
-)
-
-#Pathway-level communication tables
-met_paths_pathway <- subsetCommunication(
-  metastatic_CellChat,
-  slot.name = "netP"
-)
-
-prim_paths_pathway <- subsetCommunication(
-  primary_CellChat,
-  slot.name = "netP"
-)
-
-
-#F: Aggregate networks
-
-metastatic_CellChat <- aggregateNet(
-  metastatic_CellChat
-)
-
-primary_CellChat <- aggregateNet(
-  primary_CellChat
-)
-
-
-#G: Initial network plots for each condition
-
-#Metastatic
-groupSize <- as.numeric(
-  table(metastatic_CellChat@idents)
-)
-
-netVisual_circle(
-  metastatic_CellChat@net$weight,
-  vertex.weight = groupSize,
-  weight.scale = TRUE,
-  label.edge = FALSE,
-  title.name = "Interaction weights/strength - Metastatic"
-)
-
-#Primary
-groupSize <- as.numeric(
-  table(primary_CellChat@idents)
-)
-
-netVisual_circle(
-  primary_CellChat@net$weight,
-  vertex.weight = groupSize,
-  weight.scale = TRUE,
-  label.edge = FALSE,
-  title.name = "Interaction weights/strength - Primary"
-)
-
-
-#H: Compute network centrality
+#B: Compute network centrality
 #Needed for signaling-role analyses
 
 primary_CellChat <- netAnalysis_computeCentrality(
@@ -230,7 +40,7 @@ metastatic_CellChat <- netAnalysis_computeCentrality(
 )
 
 
-#I: Merge primary and metastatic CellChat objects
+#C: Merge primary and metastatic CellChat objects
 
 object.list <- list(
   Primary = primary_CellChat,
@@ -242,8 +52,10 @@ cellchat <- mergeCellChat(
   add.names = names(object.list)
 )
 
+cellchat
 
-#J: Compare total number and strength of interactions
+
+#D: Compare total number and strength of interactions
 
 gg1 <- compareInteractions(
   cellchat,
@@ -261,7 +73,14 @@ gg2 <- compareInteractions(
 gg1 + gg2
 
 
-#K: Differential interaction networks
+#E: Differential interaction networks
+#Compare primary vs metastatic interaction number and strength
+
+pdf(
+  "differential_interactions.pdf",
+  width = 16,
+  height = 8
+)
 
 par(
   mfrow = c(1, 2),
@@ -271,46 +90,50 @@ par(
 #Difference in number of interactions
 netVisual_diffInteraction(
   cellchat,
-  weight.scale = TRUE
+  weight.scale = TRUE,
+  vertex.label.cex = 0.5
 )
 
 #Difference in interaction strength
 netVisual_diffInteraction(
   cellchat,
   weight.scale = TRUE,
-  measure = "weight"
+  measure = "weight",
+  vertex.label.cex = 0.5
 )
 
-par(
-  mfrow = c(1, 1)
-)
+dev.off()
 
 
-#L: Differential interaction heatmaps
+#F: Differential interaction heatmaps
 
-#Number of interactions
-ht_count <- netVisual_heatmap(
+gg1 <- netVisual_heatmap(
   cellchat
 )
 
-#Interaction strength
-ht_weight <- netVisual_heatmap(
+gg2 <- netVisual_heatmap(
   cellchat,
   measure = "weight"
 )
 
-ht_count + ht_weight
+gg1 + gg2
 
 
-#M: Primary vs metastatic circle plots using same scale
+#G: Primary vs metastatic circle plots
+#Use the same scale across conditions
 
-#Number of interactions
 weight.max <- getMaxWeight(
   object.list,
   attribute = c(
     "idents",
     "count"
   )
+)
+
+pdf(
+  "number_of_interactions_primary_vs_metastatic.pdf",
+  width = 16,
+  height = 8
 )
 
 par(
@@ -326,6 +149,8 @@ for (i in 1:length(object.list)) {
     label.edge = FALSE,
     edge.weight.max = weight.max[2],
     edge.width.max = 12,
+    vertex.weight = 8,
+    vertex.label.cex = 0.5,
     title.name = paste0(
       "Number of interactions - ",
       names(object.list)[i]
@@ -333,47 +158,10 @@ for (i in 1:length(object.list)) {
   )
 }
 
-par(
-  mfrow = c(1, 1)
-)
+dev.off()
 
 
-#Interaction strength
-weight.max <- getMaxWeight(
-  object.list,
-  attribute = c(
-    "idents",
-    "weight"
-  )
-)
-
-par(
-  mfrow = c(1, 2),
-  xpd = TRUE
-)
-
-for (i in 1:length(object.list)) {
-
-  netVisual_circle(
-    object.list[[i]]@net$weight,
-    weight.scale = TRUE,
-    label.edge = FALSE,
-    edge.weight.max = weight.max[2],
-    edge.width.max = 12,
-    title.name = paste0(
-      "Interaction strength - ",
-      names(object.list)[i]
-    )
-  )
-}
-
-par(
-  mfrow = c(1, 1)
-)
-
-
-#N: Overall outgoing vs incoming signaling roles
-#This generated the Primary vs Metastatic scatter plot
+#H: Overall outgoing vs incoming signaling roles
 
 num.link <- sapply(
   object.list,
@@ -389,11 +177,11 @@ weight.MinMax <- c(
   max(num.link)
 )
 
-gg_role <- list()
+gg <- list()
 
 for (i in 1:length(object.list)) {
 
-  gg_role[[i]] <- netAnalysis_signalingRole_scatter(
+  gg[[i]] <- netAnalysis_signalingRole_scatter(
     object.list[[i]],
     title = names(object.list)[i],
     weight.MinMax = weight.MinMax
@@ -401,160 +189,11 @@ for (i in 1:length(object.list)) {
 }
 
 patchwork::wrap_plots(
-  plots = gg_role
+  plots = gg
 )
 
 
-#O: Cell-type-specific changes in signaling
-
-#Myeloid
-gg_myeloid <- netAnalysis_signalingChanges_scatter(
-  cellchat,
-  idents.use = "Myeloid Cell"
-)
-
-print(gg_myeloid)
-
-
-#T/NK
-gg_TNK <- netAnalysis_signalingChanges_scatter(
-  cellchat,
-  idents.use = "T/NK Cell"
-)
-
-print(gg_TNK)
-
-
-#Alveolar epithelial cells
-gg_lung <- netAnalysis_signalingChanges_scatter(
-  cellchat,
-  idents.use = "Alveolar epithelial cells (Lung)"
-)
-
-print(gg_lung)
-
-
-#Myoblast
-gg_myoblast <- netAnalysis_signalingChanges_scatter(
-  cellchat,
-  idents.use = "Myoblast"
-)
-
-print(gg_myoblast)
-
-
-#Malignant subcluster 2
-gg_sub2 <- netAnalysis_signalingChanges_scatter(
-  cellchat,
-  idents.use = "Osteoblast_subcluster_2"
-)
-
-print(gg_sub2)
-
-
-#Malignant subcluster 5
-gg_sub5 <- netAnalysis_signalingChanges_scatter(
-  cellchat,
-  idents.use = "Osteoblast_subcluster_5"
-)
-
-print(gg_sub5)
-
-
-#P: Compare signaling pathway information flow
-
-gg_rank <- rankNet(
-  cellchat,
-  mode = "comparison",
-  stacked = TRUE,
-  do.stat = TRUE
-)
-
-print(gg_rank)
-
-
-#Q: Signaling-role heatmaps
-#Outgoing, incoming, and overall signaling
-
-pathway.union <- union(
-  object.list[[1]]@netP$pathways,
-  object.list[[2]]@netP$pathways
-)
-
-
-#Outgoing signaling
-ht_out_primary <- netAnalysis_signalingRole_heatmap(
-  object.list[[1]],
-  pattern = "outgoing",
-  signaling = pathway.union,
-  title = "Primary"
-)
-
-ht_out_metastatic <- netAnalysis_signalingRole_heatmap(
-  object.list[[2]],
-  pattern = "outgoing",
-  signaling = pathway.union,
-  title = "Metastatic"
-)
-
-ComplexHeatmap::draw(
-  ht_out_primary + ht_out_metastatic,
-  ht_gap = grid::unit(
-    0.5,
-    "cm"
-  )
-)
-
-
-#Incoming signaling
-ht_in_primary <- netAnalysis_signalingRole_heatmap(
-  object.list[[1]],
-  pattern = "incoming",
-  signaling = pathway.union,
-  title = "Primary"
-)
-
-ht_in_metastatic <- netAnalysis_signalingRole_heatmap(
-  object.list[[2]],
-  pattern = "incoming",
-  signaling = pathway.union,
-  title = "Metastatic"
-)
-
-ComplexHeatmap::draw(
-  ht_in_primary + ht_in_metastatic,
-  ht_gap = grid::unit(
-    0.5,
-    "cm"
-  )
-)
-
-
-#Overall signaling
-ht_all_primary <- netAnalysis_signalingRole_heatmap(
-  object.list[[1]],
-  pattern = "all",
-  signaling = pathway.union,
-  title = "Primary"
-)
-
-ht_all_metastatic <- netAnalysis_signalingRole_heatmap(
-  object.list[[2]],
-  pattern = "all",
-  signaling = pathway.union,
-  title = "Metastatic"
-)
-
-ComplexHeatmap::draw(
-  ht_all_primary + ht_all_metastatic,
-  ht_gap = grid::unit(
-    0.5,
-    "cm"
-  )
-)
-
-
-#R: Functional signaling similarity
+#I: Functional signaling similarity
 
 cellchat <- computeNetSimilarityPairwise(
   cellchat,
@@ -577,19 +216,255 @@ netVisual_embeddingPairwise(
   label.size = 3.5
 )
 
-rankSimilarity(
+
+#J: Structural signaling similarity
+
+cellchat <- computeNetSimilarityPairwise(
   cellchat,
-  type = "functional"
+  type = "structural"
+)
+
+cellchat <- netEmbedding(
+  cellchat,
+  type = "structural"
+)
+
+cellchat <- netClustering(
+  cellchat,
+  type = "structural"
+)
+
+netVisual_embeddingPairwise(
+  cellchat,
+  type = "structural",
+  label.size = 3.5
 )
 
 
-#S: Pathway-specific follow-up
-#Example: VEGF
+#K: Compare signaling pathway information flow
 
-pathways.show <- c("VEGF")
+gg1 <- rankNet(
+  cellchat,
+  mode = "comparison",
+  stacked = TRUE,
+  do.stat = TRUE
+)
+
+gg2 <- rankNet(
+  cellchat,
+  mode = "comparison",
+  stacked = FALSE,
+  do.stat = TRUE
+)
+
+combined <- gg1 + gg2
+
+ggsave(
+  "rankNet_primary_vs_metastatic.pdf",
+  combined,
+  width = 16,
+  height = 18
+)
 
 
-#VEGF circle plots with matched scale
+#L: Signaling-role heatmaps
+#Compare outgoing, incoming, and overall signaling
+
+i <- 1
+
+pathway.union <- union(
+  object.list[[i]]@netP$pathways,
+  object.list[[i + 1]]@netP$pathways
+)
+
+
+#Outgoing signaling
+
+ht1 <- netAnalysis_signalingRole_heatmap(
+  object.list[[i]],
+  pattern = "outgoing",
+  signaling = pathway.union,
+  title = names(object.list)[i],
+  width = 5,
+  height = 12,
+  font.size = 6
+)
+
+ht2 <- netAnalysis_signalingRole_heatmap(
+  object.list[[i + 1]],
+  pattern = "outgoing",
+  signaling = pathway.union,
+  title = names(object.list)[i + 1],
+  width = 5,
+  height = 12,
+  font.size = 6
+)
+
+#Shrink pathway labels
+ht1@row_names_param$gp <- grid::gpar(
+  fontsize = 4
+)
+
+ht2@row_names_param$gp <- grid::gpar(
+  fontsize = 4
+)
+
+#Shrink cell-type labels
+ht1@column_names_param$gp <- grid::gpar(
+  fontsize = 6
+)
+
+ht2@column_names_param$gp <- grid::gpar(
+  fontsize = 6
+)
+
+pdf(
+  "outgoing_signaling_role_heatmap.pdf",
+  width = 16,
+  height = 22
+)
+
+draw(
+  ht1 + ht2,
+  ht_gap = unit(
+    0.5,
+    "cm"
+  )
+)
+
+dev.off()
+
+
+#Incoming signaling
+
+ht1 <- netAnalysis_signalingRole_heatmap(
+  object.list[[i]],
+  pattern = "incoming",
+  signaling = pathway.union,
+  title = names(object.list)[i],
+  width = 5,
+  height = 12,
+  color.heatmap = "GnBu"
+)
+
+ht2 <- netAnalysis_signalingRole_heatmap(
+  object.list[[i + 1]],
+  pattern = "incoming",
+  signaling = pathway.union,
+  title = names(object.list)[i + 1],
+  width = 5,
+  height = 12,
+  color.heatmap = "GnBu"
+)
+
+#Shrink pathway labels
+ht1@row_names_param$gp <- grid::gpar(
+  fontsize = 4
+)
+
+ht2@row_names_param$gp <- grid::gpar(
+  fontsize = 4
+)
+
+#Shrink cell-type labels
+ht1@column_names_param$gp <- grid::gpar(
+  fontsize = 6
+)
+
+ht2@column_names_param$gp <- grid::gpar(
+  fontsize = 6
+)
+
+pdf(
+  "incoming_signaling_role_heatmap.pdf",
+  width = 16,
+  height = 22
+)
+
+draw(
+  ht1 + ht2,
+  ht_gap = unit(
+    0.5,
+    "cm"
+  )
+)
+
+dev.off()
+
+
+#Overall signaling
+
+ht1 <- netAnalysis_signalingRole_heatmap(
+  object.list[[i]],
+  pattern = "all",
+  signaling = pathway.union,
+  title = names(object.list)[i],
+  width = 5,
+  height = 12,
+  color.heatmap = "OrRd"
+)
+
+ht2 <- netAnalysis_signalingRole_heatmap(
+  object.list[[i + 1]],
+  pattern = "all",
+  signaling = pathway.union,
+  title = names(object.list)[i + 1],
+  width = 5,
+  height = 12,
+  color.heatmap = "OrRd"
+)
+
+#Shrink pathway labels
+ht1@row_names_param$gp <- grid::gpar(
+  fontsize = 4
+)
+
+ht2@row_names_param$gp <- grid::gpar(
+  fontsize = 4
+)
+
+#Shrink cell-type labels
+ht1@column_names_param$gp <- grid::gpar(
+  fontsize = 6
+)
+
+ht2@column_names_param$gp <- grid::gpar(
+  fontsize = 6
+)
+
+pdf(
+  "all_signaling_role_heatmap.pdf",
+  width = 16,
+  height = 22
+)
+
+draw(
+  ht1 + ht2,
+  ht_gap = unit(
+    0.5,
+    "cm"
+  )
+)
+
+dev.off()
+
+
+#M: Cell-type / ligand-receptor comparison
+#Example comparison used during follow-up analysis
+
+netVisual_bubble(
+  cellchat,
+  sources.use = 4,
+  targets.use = c(5:11),
+  comparison = c(1, 2),
+  angle.x = 45
+)
+
+
+#N: MHC-II pathway-specific follow-up
+
+pathways.show <- c("MHC-II")
+
 weight.max <- getMaxWeight(
   object.list,
   slot.name = "netP",
@@ -609,6 +484,7 @@ for (i in 1:length(object.list)) {
     layout = "circle",
     edge.weight.max = weight.max[1],
     edge.width.max = 10,
+    vertex.label.cex = 0.6,
     signaling.name = paste(
       pathways.show,
       names(object.list)[i]
@@ -621,42 +497,35 @@ par(
 )
 
 
-#VEGF heatmaps
-ht_vegf <- list()
+#O: Example cell-type-specific pathway follow-up
+#CD99 signaling received by myoblasts
 
-for (i in 1:length(object.list)) {
+#Find myoblast index if needed
+myoblast.idx <- which(
+  levels(object.list[[1]]@idents) == "Myoblast"
+)
 
-  ht_vegf[[i]] <- netVisual_heatmap(
-    object.list[[i]],
-    signaling = "VEGF",
-    color.heatmap = "Reds",
-    title.name = paste(
-      "VEGF signaling",
-      names(object.list)[i]
-    )
-  )
-}
+gg_cd99_receiver <- netVisual_bubble(
+  cellchat,
+  sources.use = 1:length(
+    levels(object.list[[1]]@idents)
+  ),
+  targets.use = myoblast.idx,
+  signaling = "CD99",
+  comparison = c(1, 2),
+  angle.x = 45,
+  remove.isolate = TRUE
+)
 
-ComplexHeatmap::draw(
-  ht_vegf[[1]] + ht_vegf[[2]],
-  ht_gap = grid::unit(
-    0.5,
-    "cm"
-  )
+ggsave(
+  "Myoblast_CD99_receiver.pdf",
+  gg_cd99_receiver,
+  width = 10,
+  height = 7
 )
 
 
-#T: Save CellChat objects
-
-saveRDS(
-  metastatic_CellChat,
-  file = "metastatic_CellChat.rds"
-)
-
-saveRDS(
-  primary_CellChat,
-  file = "primary_CellChat.rds"
-)
+#P: Save merged CellChat comparison object
 
 saveRDS(
   cellchat,
